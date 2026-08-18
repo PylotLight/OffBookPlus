936e69e3329a8f9db660992bfda6509a107c33a4
Light <lochois@gmail.com>
2026-08-18 06:21:44 +0000

    Bump minSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.devlight.offbookplus"
-        minSdk = 33
+        minSdk = 35
         targetSdk = 37
         versionCode = 1
         versionName = "0.0.0"
